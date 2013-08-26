a692389da441bc3751cb25eb08dab0df1e00e5f8
Philip Breslin <philipbreslin@hotmail.com>
2013-08-26 14:04:47 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pragbookself::Application.config.secret_token = 'a2856d88474af97c560249b692a6fa6c88c96609b2951e6a0ca56f0e2fc7175b5a62ad8238dbf4d91f19815f6b918cfd7deba91c6cfff0ac3cd02bc32090688e'
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,23 @@
 Pragbookself::Application.routes.draw do
   get 'admin' => 'admin#index'
-  controller :sessions do
+    controller :sessions do
     get 'login' => :new
-
     post 'login' => :create
     delete 'logout' => :destroy
-  end
-  scope '(:locale)' do
+    end
+
     resources :users
     resources :orders
     resources :line_items
     resources :carts
+    get "store/index"
     resources :products do
       get :who_bought, on: :member
     end
+# ...
+# You can have the root of your site routed with "root"
+# just remember to delete public/index.html.
+# root :to => 'welcome#index'
     root to: 'store#index', as: 'store'
-    end
-end
+# ...
+  end
